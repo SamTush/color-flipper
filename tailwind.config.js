36c6743c0182bb0237b0b6352234777843d137dd
Samson Tush <samo4421@icloud.com>
2023-06-07 14:56:57 +0300

    Add layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            
-        }
+          myRed: '#b20d30',
+          myBlack: '#0f110c',
+          myGreen: '#5b8266',
+          myPink: '#eb9486',
+          myCoral: '#fc814a',
+          myOrange: '#ffb140'
+        },
+        fontFamily: {
+            myFont:['"Geologica"', 'sans-serif'],
+            cherryBomb: ['"Cherry Bomb One"', 'cursive'],
+        },
+        spacing: {
+            '555': '100vh',
+        },
     },
   },
   plugins: [],
